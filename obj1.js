b29fe51dca7343d6f466e5381540b9558c00d03c
Marianna1903 <84705083+Marianna1903@users.noreply.github.com>
2021-10-04 16:50:29 +0400

    Add files via upload

---
+++ obj1.js
@@ -0,0 +1,166 @@
+// let obj = {
+// 	"surname": ["Margaryan", "Babayan", "Tumanyan"],
+// 	"age": [17, 24, 29],
+// 	"city": ["Erevan","Erevan","Gyumri"],
+
+// }
+
+// for(var i=0;i<obj.surname.length;i++){
+// 	if(obj.age[i]>=20 && obj.city[i]=="Erevan"){
+// 		console.log(obj.surname[i]);
+// 	}
+// }
+
+// let obj = {
+// 	"name":["Ani","Anna","Mari"],
+// 	"surname":["Ananyan","Movsisyan","Mkrtchyan"],
+// 	"number":[5,14,19],
+
+// }
+// for(var i=0;i<obj.name.length;i++){
+// 	if(obj.number[i]>=11 && obj.number[i]<=20 && obj.name[i][0]=="A"){
+// 		console.log(obj.name[i]);
+// 	}
+// }
+
+// let x = 0;
+// let obj = {
+// 	"hexinak":["Ananyan","Abrahamyan","Tumanyan"],
+// 	"ejer":[45,62,90],
+// }
+// for(var i=0;i<obj.hexinak.length;i++){
+// 	if(obj.hexinak[i][0]=="A"){
+// 		x += obj.ejer[i];
+// 	}
+// }
+// console.log(x)
+
+
+// let obj = {
+// 	"hexinak":["Ananyan","Abrahamyan","Tumanyan"],
+// 	"anun":["hayoc patmutyun","qimia","hayoc ekexecu patmutyun"],
+// }
+// for(var i=0;i<obj.hexinak.length;i++){
+// 	if(obj.anun[i].includes("patmutyun")==true){
+// 		console.log(obj.hexinak[i])
+// 	}
+// }
+
+// let x = 0;
+// let obj = {
+// 	"hexinak":["Ananyan","Abrahamyan","Tumanyan"],
+// 	"ejer":[35,43,74],
+// 	"gin":[4000,5000,7000],
+// }
+// for(var i=0;i<obj.hexinak.length;i++){
+// 	if(obj.ejer[i]>40){
+// 		console.log(obj.hexinak[i]+" "+obj.gin[i]*2);
+// 	}else{
+// 		console.log(obj.hexinak[i]+" "+obj.gin[i]);
+// 	}
+
+// }
+
+
+// let obj = {
+// 	"name":["lurer","film","multfilm"],
+// 	"hour":[21,18,14],
+// 	"minute":[10,20,50],
+// }
+// for(var i=0;i<obj.name.length;i++){
+// 	if(obj.hour[i]==18 && obj.minute[i]==20){
+// 		console.log(obj.name[i]);
+// 	}
+// }
+
+// let obj = {
+// 	"name":["lurer","film","multfilm"],
+// 	"hour":[21,18,19],
+// 	"minute":[10,20,50],
+// }
+// for(var i=0;i<obj.name.length;i++){
+// 	if(obj.hour[i]>=19 && obj.minute[i]>=30 || obj.hour[i]>19 && obj.minute[i]>0){
+// 		console.log(obj.name[i]);
+// 	}
+// }
+
+// let obj = {
+// 	"name":["Moskva","Kinohayastan","Nairi"],
+// 	"films":["Moana","Film","Moana"],
+// }
+// for(var i=0;i<obj.name.length;i++){
+// 	if(obj.films[i]=="Moana"){
+// 		console.log(obj.name[i]);
+// 	}
+// }
+
+// let obj = {
+// 	"ararka":["qimia","matematika","angleren"],
+// 	"reyting":[14,17,11],
+// 	"qnnakan":[16,18,13],
+// }
+// for(var i=0;i<obj.ararka.length;i++){
+// 	if(obj.reyting[i]>15 && obj.qnnakan[i]>14){
+// 		console.log(obj.ararka[i])
+// 	}
+// }
+
+
+// let obj = {
+// 	"amsativ":[14,21,25],
+// 	"shabatva_or":["shabat","shabat","hingshabti"],
+// }
+// for(var i=0;i<obj.amsativ.length;i++){
+// 	if(obj.shabatva_or[i]=="shabat"){
+// 		console.log(obj.amsativ[i]);
+// 		}
+// }
+
+///////////////////////////////////////////////////////////
+
+// let us = {
+// 	"name":"Anna",
+// 	"surname":"Sargsyan",
+// 	"matem":67,
+// 	"otar":74,
+// 	"kensab":81,
+// 	"fizika":76,
+// 	"inform":89,
+// }
+// let x = us.matem+us.otar+us.kensab+us.fizika+us.inform;
+// if(x/5>=80){
+// 	alert("gerazanc");
+// }else if(x/5>=70){
+// 	alert("lav");
+// }else if(x/5>=40){
+// 	alert("bavarar");
+// }else if(x/5<=40){
+// 	alert("heracnel buhic")
+// }
+
+
+// let human = {
+// 	"name":"Lilit",
+// 	"surname":"Lalayan",
+// 	"age":11,
+// 	"ser":"igakan",
+// }
+// if(human.ser = "igakan"){
+// 	if(human.age>1 && human.age<18){
+// 		alert("poqr axjik");
+// 	}else if(human.age>17 && human.age<64){
+// 		alert("chapahas mard");
+// 	}else if(human.age>63){
+// 		alert("tatik");
+// 	}
+// }
+// else if(human.ser = "arakan"){
+// 	if(human.age>1 && human.age<18){
+// 		alert("poqr txa");
+// 	}else if(human.age>17 && human.age<64){
+// 		alert("chapahas mard");
+// 	}else if(human.age>63){
+// 		alert("papik");
+// 	}
+// }
+
